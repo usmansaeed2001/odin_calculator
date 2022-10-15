2bb2e09b3a205027824b39c76735d5b59c003316
usmansaeed2001 <muawan2001@gmail.com>
2022-10-15 23:16:40 +0500

    Added style

--- script.js
+++ script.js
@@ -1,12 +1,57 @@
-function display(item) {
-        alert(`You pressed ${item}`);
+class Calculator {
+        constructor(previousOperandandTextElement, currentOperandandTextElement) {
+                this.previousOperandandTextElement = previousOperandandTextElement
+                this.currentOperandandTextElement = currentOperandandTextElement
+                this.clear()
+                alert('calc created')
+        }
+
+        clear() {
+                this.currentOperand = '24'
+                this.previousOperand = ''
+                this.operation = undefined
+        }
+
+        delete() {
+
+        }
+        appendNumber(number) {
+                this.currentOperand = this.currentOperand.toString() + number.toString()
+
+        }
+
+        chooseOperation(operation) {
+
+        }
+        compute() {
+
+        }
+        updateDisplay() {
+                this.currentOperandandTextElement.innerText = this.currentOperand
+        }
 }
 
-let touch = document.querySelectorAll('digit');
+alert('wowowow')
+
+
+const numberButton = document.querySelectorAll('[data-number]')
+const operationButtons = document.querySelectorAll('[data-operation]')
+const equalsButton = document.querySelector('[data-equals]')
+const deleteButton = document.querySelector('[data-delete]')
+const allClearButton = document.querySelector('[data-all-clear]')
+const previousOperandandTextElement = document.querySelector('[data-previous-operand]')
+const currentOperandandTextElement = document.querySelector('[data-current-operand]')
+alert('wow')
 
 
 
+const Calculator = new Calculator(previousOperandandTextElement, currentOperandandTextElement)
 
-touch.forEach(addEventListener('click', display));
+numberButton.forEach(button => {
+        button.addEventListener('click', ()=> {
+                Calculator.appendNumber(button.innerText)
+                Calculator.updateDisplay()
+        })
+})
 
 
