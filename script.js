c5c21be29bc901f0964b8c05000c11d4c81c7f8c
usmansaeed2001 <muawan2001@gmail.com>
2022-10-16 00:04:25 +0500

    working calc

--- script.js
+++ script.js
@@ -3,35 +3,92 @@ class Calculator {
                 this.previousOperandandTextElement = previousOperandandTextElement
                 this.currentOperandandTextElement = currentOperandandTextElement
                 this.clear()
-                alert('calc created')
         }
 
         clear() {
-                this.currentOperand = '24'
+                this.currentOperand = ''
                 this.previousOperand = ''
                 this.operation = undefined
         }
 
         delete() {
-
+                this.currentOperand = this.currentOperand.toString().slice(0, -1)
         }
         appendNumber(number) {
+                if (number === '.' && this.currentOperand.includes('.')) return
                 this.currentOperand = this.currentOperand.toString() + number.toString()
 
         }
 
         chooseOperation(operation) {
-
+                if (this.currentOperand === '') return
+                if (this.previousOperand != '') {
+                        this.compute()
+                }
+                this.operation = operation
+                this.previousOperand = this.currentOperand
+                this.currentOperand = ''
         }
         compute() {
+                let computation
+                const prev = parseFloat(this.previousOperand)
+                const current = parseFloat(this.currentOperand)
+                if (isNaN(prev) || isNaN(current)) return
+                switch (this.operation) {
+                        case '+':
+                                computation = prev + current
+                                break
+                        
+                        case '-':
+                                computation = prev - current
+                                break
+                        
+                        case 'x':
+                                computation = prev * current
+                                break
+                        
+                        case '/':
+                                computation = prev / current
+                                break
+                        default:
+                                return
+                        
+                }
+                this.currentOperand = computation
+                this.operation = undefined
+                this.previousOperand = ''
 
         }
+
+
+        getDisplayNumber(number) {
+                const stringNumber = number.toString()
+                const integerDigits = parseFloat(stringNumber.split('.')[0])
+                const decimalDigits = stringNumber.split('.')[1]
+               let integerDisplay
+               if (isNaN(integerDigits)) {
+                integerDisplay = ''
+               } else {
+                integerDisplay = integerDigits.toLocaleString('en', {
+                        maximumFractionDigits: 0
+                })
+               }
+               if (decimalDigits != null) {
+                return `${integerDisplay}.${decimalDigits}`
+               } else {
+                return integerDisplay
+               }
+        }
+
+
         updateDisplay() {
-                this.currentOperandandTextElement.innerText = this.currentOperand
+                this.currentOperandandTextElement.innerText = this.getDisplayNumber(this.currentOperand)
+               if (this.operation != null) {
+                this.previousOperandandTextElement.innerText = 
+                `${this.getDisplayNumber(this.previousOperand)} ${this.operation}`
+               }
         }
 }
-
-alert('wowowow')
 
 
 const numberButton = document.querySelectorAll('[data-number]')
@@ -41,17 +98,37 @@ const deleteButton = document.querySelector('[data-delete]')
 const allClearButton = document.querySelector('[data-all-clear]')
 const previousOperandandTextElement = document.querySelector('[data-previous-operand]')
 const currentOperandandTextElement = document.querySelector('[data-current-operand]')
-alert('wow')
 
 
 
-const Calculator = new Calculator(previousOperandandTextElement, currentOperandandTextElement)
+const calculator = new Calculator(previousOperandandTextElement, currentOperandandTextElement)
 
 numberButton.forEach(button => {
         button.addEventListener('click', ()=> {
-                Calculator.appendNumber(button.innerText)
-                Calculator.updateDisplay()
+                calculator.appendNumber(button.innerText)
+                calculator.updateDisplay()
+        })
+})
+
+operationButtons.forEach(button => {
+        button.addEventListener('click', ()=> {
+                calculator.chooseOperation(button.innerText)
+                calculator.updateDisplay()
         })
 })
 
 
+equalsButton.addEventListener('click', button => {
+        calculator.compute()
+        calculator.updateDisplay()
+})
+
+allClearButton.addEventListener('click', button => {
+        calculator.clear()
+        calculator.updateDisplay()
+})
+
+deleteButton.addEventListener('click', button => {
+        calculator.delete()
+        calculator.updateDisplay()
+})
